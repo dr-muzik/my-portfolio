8720da3b28a320d1db7c71bab3be0f511a649d2c
Wisdom Peters <drmusicpiano@gmail.com>
2024-10-02 19:37:43 +0100

    fixed: removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
-import Layout from './dashboard/layout';
+// import Image from 'next/image';
+// import Layout from './dashboard/layout';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
 	redirect('/dashboard'); // Automatically redirects to /dashboard
-	return null;
 }
